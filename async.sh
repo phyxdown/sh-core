a45cbe069ef22cd02daa62e18a10e05a0b850349
phyxdown <phyxdown@qq.com>
2021-09-27 22:19:29 +0800

    add async support

---
+++ async.sh
@@ -0,0 +1,19 @@
+async() {
+    eval "$1"
+    echo "$2@-SPLIT-@$3"
+}
+
+await() {
+    file=`echo ${1%@-SPLIT-@*}`
+    message=`echo ${1#*@-SPLIT-@}`
+    DONE=0
+    while [ $DONE -eq 0 ]
+    do
+        if [ -z "`cat $file | grep \"$message\"`" ]
+        then
+            sleep 1
+        else
+            DONE=1
+        fi
+    done
+}
--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ source logger.sh
 source net.sh
 source time.sh
 source list.sh
+source async.sh
 
 info "[test no params]"
 httpget
@@ -17,6 +18,24 @@ info "[test listappend A B]"
 L=A
 L=`listappend $L "B"`
 assertequals "A B" "$L"
+info "[pass]"
+
+info "test async + await"
+future=`async "echo \"server started\" > boot.log" "boot.log" "server started"`
+assertequals "boot.log@-SPLIT-@server started" "$future"
+await "$future"
+info "[pass]"
+
+info "test multi(async + await)"
+for i in {1..2}
+do
+    future=`async "echo $i > $i.log" "$i.log" "$i"`
+    futures=`listappend "$futures" "$future"`
+done
+for i in $futures
+do
+    await "$i"
+done
 info "[pass]"
 
 info "test countdown 3"
